e3dce5055aac64830b4c7b3f641bcf5d4060ea38
CarsonS21 <sessomsc@email.sc.edu>
2026-04-15 00:47:48 -0400

    added recording and states to button

--- app/src/main/java/com/example/voicememoapp/ui/components/RecordMemo.kt
+++ app/src/main/java/com/example/voicememoapp/ui/components/RecordMemo.kt
@@ -1,31 +1,74 @@
 package com.example.voicememoapp.ui.components
 
+import android.media.MediaRecorder
+import android.os.Build
+import android.util.Log
+import android.util.Log.i
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mic
+import androidx.compose.material.icons.filled.MicOff
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 /** The button for recording memos **/
+
 @Composable
 fun RecordMemoButton(modifier: Modifier) {
+    var isRecording by remember { mutableStateOf(false) }
+    var mediaRecorder by remember { mutableStateOf<MediaRecorder?>(null) }
+    val context = LocalContext.current
+    var outputFile by remember { mutableStateOf("") }
     Column(modifier.fillMaxWidth()) {
     FloatingActionButton(
-        onClick = { /* action */ },
+        onClick = {
+            isRecording = !isRecording
+            if (isRecording){
+                outputFile = "${context.externalCacheDir?.absolutePath}/memo_${System.currentTimeMillis()}.mp3"
+                mediaRecorder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    MediaRecorder(context)
+                } else {
+                    @Suppress("DEPRECATION")
+                    MediaRecorder()
+                }.apply {
+                    setAudioSource(MediaRecorder.AudioSource.MIC)
+                    setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                    setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+                    setOutputFile(outputFile)
+                    prepare()
+                    start()
+                }
+
+            }else{
+                mediaRecorder?.apply {
+                    stop()
+                    release()
+                }
+                mediaRecorder = null
+
+                Log.d("RecordMemo", "Saved to: $outputFile")
+            }
+                  },
         containerColor = Color.Red,
         modifier = Modifier.align(Alignment.CenterHorizontally)
             .padding(16.dp)
     ) {
         Icon(
-            imageVector = Icons.Default.PlayArrow,
-            contentDescription = "Play",
+            imageVector = if (isRecording) Icons.Default.Mic else Icons.Default.PlayArrow,
+            contentDescription = if (isRecording) "Stop recording" else "Start recording",
             tint = Color.White
         )
     }
